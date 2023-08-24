50bafd94e80815c69c4c6d2cdd7a1c61137b6a08
guapjorge <alcalajorgea@gmail.com>
2023-08-23 21:20:49 -0700

    first push

--- client/server/models/User.js
+++ client/server/models/User.js
@@ -1,1 +1,21 @@
 const express = require('espress')
+
+const profileSchema = new schema (
+    {
+        first: String,
+        Last: String,
+        age: Number,
+        expenses: [
+            {
+            amount: Number,
+            description: String,
+            dataCreated: Date,
+            dueDate: Date
+            },
+        ],
+        toJSON: {
+            virtuals: true,
+        },
+        id:false,
+        }
+);
