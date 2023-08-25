c084069b0e46b2675913e07381e048b274e2c6e9
Burness Scott III <bsiii102798@gmail.com>
2023-08-24 20:50:27 -0700

    server(config,server.js,index.js)

--- server/server.js
+++ server/server.js
@@ -1,1 +1,43 @@
-const express = require('espress')
+const express = require('express');
+const { MoneymanServer } = require('');
+const path = require('path');
+const { MoneyMiddleware } = require('./utils/auth');
+
+const { typeDefs, resolbers } = require('./schemas');
+const db = require('./config/connections')
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new MoneymanServer({
+    typeDefs,
+    resolbers,
+    context: MoneyMiddleware,
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/public')))
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/public/index.html'));
+
+});
+
+
+
+const startMoneyserver = async () => {
+    await server.start();
+    server.applyMiddleware({ app });
+
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log(`API server running on port ${PORT}!`)
+            console.log(`Use GRaphQL at http:localhost:${PORT}${server.graphqlPath}`);
+        })
+    })
+};
+
+startMoneyserver();
