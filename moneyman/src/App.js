9fffaecdd60f84410578a2a97820b31548809df1
ARS2989 <raysoloriojr@gmail.com>
2023-08-31 18:06:29 -0700

    app.js API call from front end

--- moneyman/src/App.js
+++ moneyman/src/App.js
@@ -26,3 +26,32 @@ function App() {
 }
 
 export default App;
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'; // Import Axios for making API requests
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Make an API call to fetch data from the backend
+    axios.get('/api/data')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      {/* Display fetched data */}
+      {data.map(item => (
+        <p key={item.id}>{item.name}</p>
+      ))}
+    </div>
+  );
+}
+
+export default App;
