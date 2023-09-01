ca32e0a26ed3d5996b4e5b126597a277c518289a
Burness Scott III <bsiii102798@gmail.com>
2023-08-31 18:10:52 -0700

    components

--- moneyman/src/components/LoginForm.js
+++ moneyman/src/components/LoginForm.js
@@ -5,15 +5,45 @@ function LoginForm() {
     const [password, setPassword] = useState('');
 
     const handleLogin = () => {
-        // Send login data to the server
-        // Handle authentication
+
+        const loginData = {
+            email: email,
+            password: password
+        };
+
+        sendDataToServer(loginData)
+            .then(response => {
+                console.log('User logged in successfully:', response);
+            })
+            .catch(error => {
+                console.error('Error logging in:', error);
+            });
     };
 
     return (
         <div>
             <h2>Login</h2>
-            {/* Form for email and password */}
-            <button onClick={handleLogin}>Login</button>
+            <form>
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                />
+
+                <label htmlFor="password">Password:</label>
+                <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
+
+                <button type="button" onClick={handleLogin}>
+                    Login
+                </button>
+            </form>
         </div>
     );
 }
